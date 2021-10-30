c0237d4806641ae0ce568ffba0ee79eea5d9952c
/saad/zizi <saad_zizi@hotmail.com>
2021-10-30 20:33:36 +0200

    Create initialize.php

---
+++ bullShitApi/core/initialize.php
@@ -0,0 +1,13 @@
+<?php
+
+    defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+    defined('SITE_ROOT') ? null : define('SITE_ROOT',DS . 'var' . DS . 'www' . DS . 'html' . DS . 'api_php');
+    defined('INC_PATH') ? null : define('INC_PATH', SITE_ROOT.DS.'includes');
+    defined('CORE_PATH') ? null : define('CORE_PATH', SITE_ROOT.DS.'core');
+
+
+    //load the config file 
+    require_once(INC_PATH.DS.'config.php');
+
+    //core classes
+    require_once(CORE_PATH.DS.'post.php');
